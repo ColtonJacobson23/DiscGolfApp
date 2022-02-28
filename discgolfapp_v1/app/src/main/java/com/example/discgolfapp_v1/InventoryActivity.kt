ed11392702fe4747701441349d5acfcb1e8d58f0
FattHoe <hoe000411@gmail.com>
2022-02-27 22:10:59 -0600

    Created expandable list to display discs by categories.

--- discgolfapp_v1/app/src/main/java/com/example/discgolfapp_v1/InventoryActivity.kt
+++ discgolfapp_v1/app/src/main/java/com/example/discgolfapp_v1/InventoryActivity.kt
@@ -1,11 +1,42 @@
 package com.example.discgolfapp_v1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ExpandableListAdapter
+import android.widget.ExpandableListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.discgolfapp_v1.ui.main.CustomExpandableListAdapter
+import com.example.discgolfapp_v1.ui.main.VirtualBagData.data
 
 class InventoryActivity : AppCompatActivity() {
+    private var expandableListView: ExpandableListView? = null
+    private var adapter: ExpandableListAdapter? = null
+    private var titleList: List<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventory)
+
+        expandableListView = findViewById(R.id.virtualBagList)
+        if (expandableListView != null) {
+            val listData = data
+            titleList = ArrayList(listData.keys)
+            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+            expandableListView!!.setAdapter(adapter)
+
+            expandableListView!!.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
+                Toast.makeText(
+                    applicationContext,
+                    "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
+                            titleList as
+                                    ArrayList<String>
+                            )
+                            [groupPosition]]!!.get(
+                        childPosition
+                    ),
+                    Toast.LENGTH_SHORT
+                ).show()
+                false
+            }
+        }
     }
 }
---
+++ discgolfapp_v1/app/src/main/java/com/example/discgolfapp_v1/ui/main/VirtualBagData.kt
@@ -0,0 +1,27 @@
+package com.example.discgolfapp_v1.ui.main
+
+import java.util.*
+
+internal object VirtualBagData {
+    val data: HashMap<String, List<String>>
+        get() {
+            val expandableListDetail = HashMap<String, List<String>>()
+
+            val distanceDrivers: MutableList<String> = ArrayList()
+            val fairwayDrivers: MutableList<String> = ArrayList()
+            val midranges: MutableList<String> = ArrayList()
+            val putters: MutableList<String> = ArrayList()
+
+            distanceDrivers.add("Thunderbird")
+            fairwayDrivers.add("Leopard3")
+            midranges.add("Buzzz")
+            putters.add("Link")
+
+            expandableListDetail["Distance Drivers"] = distanceDrivers
+            expandableListDetail["Fairway Drivers"] = fairwayDrivers
+            expandableListDetail["Mid-Ranges"] = midranges
+            expandableListDetail["Putters"] = putters
+
+            return expandableListDetail
+        }
+}
