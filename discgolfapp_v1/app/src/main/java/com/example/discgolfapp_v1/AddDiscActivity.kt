af9b88f84c9908c1f7a1d58132766e50fbf8b464
FattHoe <hoe000411@gmail.com>
2022-03-15 16:00:41 -0500

    Created the remaining inputs for add disc activity. Implemented auto change disc type based on disc speed. Upload image and color picker not yet implemented.

--- discgolfapp_v1/app/src/main/java/com/example/discgolfapp_v1/AddDiscActivity.kt
+++ discgolfapp_v1/app/src/main/java/com/example/discgolfapp_v1/AddDiscActivity.kt
@@ -1,6 +1,10 @@
 package com.example.discgolfapp_v1
 
 import android.os.Bundle
+import android.view.View.OnFocusChangeListener
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -9,5 +13,61 @@ class AddDiscActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_disc)
 
+        val discSpeedInput = findViewById<EditText>(R.id.disc_speed_input)
+        discSpeedInput.onFocusChangeListener = discSpeedFocusChangeListener
     }
+
+    private val discSpeedFocusChangeListener =
+        OnFocusChangeListener { view, hasFocus ->
+            if (!hasFocus) {
+                val discSpeedInputText = (view as EditText).text.toString()
+
+                if (discSpeedInputText != "") {
+                    val discTypeDropdown = findViewById<Spinner>(R.id.disc_type_input)
+
+                    when (discSpeedInputText.toInt()) {
+                        9, 10, 11, 12, 13, 14 -> {
+                            if (discTypeDropdown.selectedItemPosition != 0) {
+                                discTypeDropdown.setSelection(0)
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Disc Type changed to 'Distance Driver'",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                        6, 7, 8 -> {
+                            if (discTypeDropdown.selectedItemPosition != 1) {
+                                discTypeDropdown.setSelection(1)
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Disc Type changed to 'Fairway Driver'",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                        4, 5 -> {
+                            if (discTypeDropdown.selectedItemPosition != 2) {
+                                discTypeDropdown.setSelection(2)
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Disc Type changed to 'Midrange'",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                        1, 2, 3 -> {
+                            if (discTypeDropdown.selectedItemPosition != 3) {
+                                discTypeDropdown.setSelection(3)
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Disc Type changed to 'Putter'",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
+            }
+        }
 }
